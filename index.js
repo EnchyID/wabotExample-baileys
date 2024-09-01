b562f74372f9301922a5ed02cd08e2370751c221
Enchy <91828204+EnchyID@users.noreply.github.com>
2024-09-02 01:12:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -47,4 +47,81 @@ async function startSock(){
   })
 }
 
+//Custom API
+
+//Example replyTo(jid, "Hello there!, msg)
+function replyTo(jid, text, msg){
+  sock.sendMessage(jid, {text: text}, {quoted: msg})
+}
+
+/**
+Example
+  sendListMessage(jid, {
+    text: "Did you wanna check my fruits?",
+    footerText: "MyBot",
+    header: null,
+    buttonText: "List of Fruits",
+    sections: [{
+      title: "This is red of fruits!",
+      highlight_label: "RED",
+      rows: [{
+        header: "Cherry",
+        title: "Section 1 of Red",
+        description: "This is fruit 1 of red",
+        id: "fruit1"
+      }, 
+        header: "Beeries",
+        title: "Section 2 of Red",
+        description: "This is fruit 2 of red",
+        id: "fruit2"
+      }]
+    }, {
+      title: "This is green of fruits!",
+      highlight_label: "GREEN",
+      rows: [{
+        header: "Apple",
+        title: "Section 1 of Green",
+        description: "This is fruit 1 of green",
+        id: "fruit3"
+      }, 
+        header: "Melon",
+        title: "Section 2 of Red",
+        description: "This is fruit 2 of green",
+        id: "fruit4"
+      }]
+    }]
+  }, msg)
+**/
+function sendListMessage(jid, options, msg){    
+    let template = generateContentMessage(from, {
+        viewOnceMessage: {
+            message: {
+                messageContextInfo: {
+                    deviceListMetadata: {},
+                    deviceListMetadataVersion: 2
+                }, 
+                interactiveMessage: {
+                    header: {
+                       type: "text",
+                       text: options.header
+                    },
+                    body: { text: options.text },
+                    footer: { text: options.footerText },
+                    nativeFlowMessage: {
+                        buttons: [{
+                            name: "single_select",
+                            buttonParamsJson: JSON.stringify({
+                                title: buttonText,
+                                sections: options.sections
+                            })
+                        }]
+                    }
+                }
+            }
+        }                      
+    }, {userJid: msg.sender, quoted: msg})
+    
+    sock.relayMessage(from, template.message, { messageId: msg.key.id });
+}
+
 startSock()
